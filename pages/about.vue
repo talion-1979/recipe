5bbb55e41f405e91e49ad0598331a22e649bb718
talion-1979 <suzukino@umich.edu>
2022-06-04 10:48:25 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <h2>{{ subtitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subtitle: 'この文書はNuxt.jsの基本的な使用方法について説明しています'
+    }
+  },
+  layout: "original"
+}
+</script>
